cb4a8a0646cf47ae64f57f4b4739acc567a56238
Paul <lecomtepaulvd@gmail.com>
2025-08-23 08:13:20 +0200

    skeleton

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
-using namespace std;
+#include <GLFW/glfw3.h>
+#include "../include/particle.h"
+#include "../include/shader.h"
 
 int main() {
+    if (!glfwInit()) return -1;
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Supernova Simulation", nullptr, nullptr);
+    if (!window) { glfwTerminate(); return -1; }
+    glfwMakeContextCurrent(window);
+
+    // Initialisation des particules et shaders
+    ParticleSystem particles;
+    Shader shader("shaders/vertex.glsl", "shaders/fragment.glsl");
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        particles.update();
+        shader.use();
+        particles.render();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
