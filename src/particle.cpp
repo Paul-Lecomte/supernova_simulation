de6fe734d5807b8e60e0d3b48ee3f0a500550623
Paul <lecomtepaulvd@gmail.com>
2025-08-23 08:13:35 +0200

    skeleton

---
+++ src/particle.cpp
@@ -0,0 +1,10 @@
+#include "../include/particle.h"
+ParticleSystem::ParticleSystem() {
+    // Initialiser les particules
+}
+void ParticleSystem::update() {
+    // Mettre à jour la position des particules
+}
+void ParticleSystem::render() {
+    // Rendu OpenGL des particules
+}
