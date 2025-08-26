2259064ef39c943faaa93e74c3dbea49da93a0f8
Paul <lecomtepaulvd@gmail.com>
2025-08-26 09:36:50 +0200

    setup particles

--- src/particle_system.cpp
+++ src/particle_system.cpp
@@ -17,4 +17,12 @@ void ParticleSystem::spawnParticle() {
 
     float angle = static_cast<float>(rand()) / RAND_MAX * 2.0f * 3.14159f;
     float speed = static_cast<float>(rand()) / RAND_MAX * 0.5f;
+
+    Particle p;
+    p.position = glm::vec2(centerX, centerY);
+    p.velocity = glm::vec2(cos(angle), sin(angle)) * speed;
+    p.color = glm::vec3(1.0f, 0.5f, 0.0f); // Orange color
+    p.life = 1.0f; // Particle life in seconds
+
+    particles.push_back(p);
 }
