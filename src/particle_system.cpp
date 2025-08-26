c92ca54aaa73e8867f7bc9af84cad1bccbb0f23c
Paul <lecomtepaulvd@gmail.com>
2025-08-26 09:33:53 +0200

    includes

--- src/particle_system.cpp
+++ src/particle_system.cpp
@@ -5,3 +5,16 @@
 #include <GLFW/glfw3.h>
 #include <GL/gl.h>
 
+// Constructor to initialize the particle system with a maximum number of particles
+ParticleSystem::ParticleSystem(unsigned int maxParticles)
+    : maxParticles(maxParticles) {
+    particles.reserve(maxParticles);
+}
+
+void ParticleSystem::spawnParticle() {
+    if (particles.size() >= maxParticles)
+        return;
+
+    float angle = static_cast<float>(rand()) / RAND_MAX * 2.0f * 3.14159f;
+    float speed = static_cast<float>(rand()) / RAND_MAX * 0.5f;
+}
